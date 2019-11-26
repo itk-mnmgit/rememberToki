b07d42a32159518f04f577e6309259eef5ae1b6d
Minamigaito Itsuki <itk.mnmgit@gmail.com>
2019-11-26 12:34:52 +0800

    DB 13個 作成
    php artisan make:migration create_テーブル名_table --create=テーブル名
    
    up(){
    $table->データ型('カラム名')->カラム修飾詞();
    
    .
    .
    }
    
    php artisan migrate

---
+++ database/migrations/2019_11_26_042449_create_group_chat_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupChatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_chat', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('user_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_chat');
+    }
+}
