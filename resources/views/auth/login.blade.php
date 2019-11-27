b7c6a4fb17a101ca3a9607ebc47a56165899058b
尾宮重宏 <sigehiro.0728@gmail.com>
2019-11-27 09:19:57 +0800

    1127
    一部変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus placeholder='メールアドレス'>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
