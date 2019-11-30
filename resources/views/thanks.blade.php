d16ab04bdb1d74648e229d22f6d8a0efab679784
尾宮重宏 <sigehiro.0728@gmail.com>
2019-11-30 18:35:53 +0800

    1130
    写真登録画面。アイコン以外OK

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -5,27 +5,48 @@
 
 @section('content')
 
+{{-- 質問１これはつけた方がいいのかデザイン崩れるdiv class="container-fluid" --}}
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <div class="row no gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" >
+                    <div class="col p-4 d-flex flex-column position-static text-center">
+                        <strong class="inline-block  text-primary">プロフィールをもっと追加しよう！
+                        <a class="btn btn-lg text-primary" href="{{ route('getMine') }}" role="button">skip▶︎▶︎</a></strong>
+                        <p class="card-text mb-auto">写真を追加して、あなたのことをもっと他のメンバーに知ってもらおう</p>
+                        {{-- 質問２ skip右に移動さしたい --}}
+                    {{-- アイコン入れる --}}
+                        {{--イツキ質問 １,自己紹介データベースに保存？  --}}
+                            <div class="form-group">
+                                    <label for="exampleInputPassword1">自己紹介を追加しましょう
+                                        <form method="post" action="sample.cgi">
+                                            <textarea name="intro" rows="4" cols="120" class="col p-4 d-flex flex-column position-static text-center"></textarea><br>
+                                                <input type="submit" value="登録">
+                                        </form>
+                                    </label>
+                            </div>
 
-<div class="row mb-2">
-    <div class="col-md-6">
-        <div class="row no gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-primary">ありがとうございました。</strong>
-                <h3 class="mb-0">登録完了しました！</h3>
-                <p class="card-text mb-auto">次は、プロフィール写真を追加しましょう。</p>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">近くの友達を見つけよう</label>
+                                <form>
+                                        <SELECT NAME="place">
+                                        <OPTION SELECTED>お住まいの地域
+                                        <OPTION VALUE="CEBU">CEBU
+                                        <OPTION VALUE="Japan">Japan
+                                        <OPTION VALUE="Korea">Korea
+                                        <OPTION VALUE="China">China
+                                        </SELECT>
+                                </form>
+                            </div>
+                                <a class="btn btn-success btn-lg text-white" href="{{ route('getMine') }}" role="button">はじめる</a>
+                    </div>
+                </div>
             </div>
-        </div>
+    </div>
 </div>
 
-</div>
-{{-- <div class="thanks-main">
-        <div class="bg-image" style="background-image: url({{ asset('image/thanksimg.jpeg') }})">
-          <div class="row">
-              <div class="col-md-4"></div>
-                    <div class="col-md-4 d-flex flex-column justify-content-center">
-                    <p class="display-4-1 text-black">登録完了しました！</p>
-                    <p class="display-4-1 text-black">さらに登録しますか？</p>
-                    </div> --}}
+
 
 <h1>introのところとUIお願いします</h1>
 
@@ -46,8 +67,8 @@
                                 <div class="col-md-6">
                                     {{-- デバッグの時だるいから今だけrequired外してます --}}
                                     {{-- <input id="picture" type="file" class="form-control{{ $errors->has('picture') ? ' is-invalid' : '' }}" name="picture" required> --}}
-                                    <input id="picture" type="file" class="form-control{{ $errors->has('picture') ? ' is-invalid' : '' }}" name="img" >
-
+                                    <input id="picture" type="file" class="form-control3{{ $errors->has('picture') ? ' is-invalid' : '' }}" name="img" >
+                            {{-- cssでform-control絡んでるから変更 --}}
                                     @if ($errors->has('picture'))
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $errors->first('picture') }}</strong>
@@ -59,10 +80,9 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        登録！
+                                        登録
                                     </button>
                                 </div>
-                                <a class="btn btn-primary btn-lg text-white" href="{{ route('getMine') }}" role="button">あとで</a>
                             </div>
 
                         </form>
