7d900aff89fcae51eba4b1338f03bfc58773a68b
平野貴之 <>
2019-12-03 11:45:44 +0800

    グループ一覧画面

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -18,7 +18,6 @@
   <a class="nav-link active text-light" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Cebu FC</a>
   @endfor
   <a class="btn btn-primary btn-lg text-white" href="{{ route('chat.listGroup') }}" role="button">＋ グループを追加する</a>
-  <button type="button" class="btn light rounded-circle p-0" style="width:2rem;height:2rem;">＋</button>
 
   <h5>ダイレクトメッセージ</h5>
 
@@ -28,7 +27,6 @@
   @endfor
 
   <a class="btn btn-primary btn-lg text-white" href="" role="button">＋ メンバーを招待する</a>
-  <button type="button" class="btn light rounded-circle p-0" style="width:2rem;height:2rem;">＋</button>
 
 </div>
 
@@ -40,25 +38,26 @@
 
   {{-- イベントカード --}}
   <div class="card mb-3" style="max-width: 540px;">
-      <div class="row no-gutters">
-        <div class="col-md-4">
-          <img src="{{ asset('image/homefootball.jpg') }}" class="card-img event-img" alt="football">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">フットサル大会</h5>
-            <p class="card-text">メトロスポーツセンターでフットサル大会を開催します！みんなで汗を流しましょう！</p>
-            <p class="card-text"><small class="text-muted">12月14日 16:00 - 20:00</small></p>
-          </div>
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img src="{{ asset('image/homefootball.jpg') }}" class="card-img event-img" alt="football">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">フットサル大会</h5>
+          <p class="card-text">メトロスポーツセンターでフットサル大会を開催します！みんなで汗を流しましょう！</p>
+          <a href="#" class="btn btn-primary">詳細</a>
+          <p class="card-text"><small class="text-muted">12月14日 16:00 - 20:00</small></p>
         </div>
       </div>
     </div>
+  </div>
 
   @endfor
 
-  <a class="btn btn-primary btn-lg text-white" href="{{ route('event.index') }}" role="button">他のイベントを見る
+  <a class="btn btn-primary btn-lg text-white" href="{{ route('event.index') }}" role="button">+ 他のイベントを見る
   </a>
-  </div>
+</div>
 
 {{-- チャットの大きな部分を表示する --}}
 <div class="chat">
