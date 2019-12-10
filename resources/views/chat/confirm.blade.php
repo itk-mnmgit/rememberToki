caa453dfe75ec6bf3280f82e18c965dcb4f4f24b
Minamigaito Itsuki <itk.mnmgit@gmail.com>
2019-12-10 17:15:10 +0800

    確認画面作成

--- resources/views/chat/confirm.blade.php
+++ resources/views/chat/confirm.blade.php
@@ -4,6 +4,64 @@
 
 @section('content')
 
+
+{{-- public function confirmGroup(Request $request)
+{
+    $group = new Group();
+
+    $imgPath = $this->saveProfileImage($request->img);
+
+    $group->name = $request->name;
+    $group->genre_id = $request->genre_id;
+    $group->user_id = Auth::user()->id;
+    $group->img = $imgPath;
+    $group->intro = $request->intro;
+
+    $genre = Genre::find($group->genre_id);
+    $user = User::find($group->user_id);
+
+    $request->session()->put('group', $group);
+
+    return view('chat.confirm', ['group' => $group, 'genre' => $genre, 'user' => $user]);
+} --}}
+
+
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="crop-card">
+                <div class="card-header">
+                    グループ作成
+                </div>
+                <form action="{{ route('chat.confirm') }}" method="post" class="form-horizontal">
+                    <div class="row">
+                        <label class="col-sm-2 control-label" for="username">名前：</label>
+                        <div class="col-sm-10">{{ $group->name }}</div>
+                    </div>
+                    <div class="row">
+                        <label class="col-sm-2 control-label" for="mail">ジャンル：</label>
+                        <div class="col-sm-10">{{  $genre }}</div>
+                    </div>
+                    <div class="row">
+                        <label class="col-sm-2 control-label" for="age">作成者：</label>
+                        <div class="col-sm-2">{{ $user }}</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <input class="btn btn-primary btn-lg text-white" type='submit' value='OK'>
+                            <a class="btn btn-primary btn-lg text-white" method='get' href="javascript:history.back()" role="button">戻る</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>ß
+
+{{-- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa --}}
+
 <h1>chat/confirm</h1>
 
 <h1>作成画面で入力したやつ表示させてます。</h1>
@@ -16,9 +74,6 @@
 
 <form  method='post' action="{{ route('chat.make') }}">
     @csrf
-    <input class="btn btn-primary btn-lg text-white" type='submit' value='OK'>
-
-    <a class="btn btn-primary btn-lg text-white" method='get' href="javascript:history.back()" role="button">戻る</a>
-
+    
 
 @endsection
