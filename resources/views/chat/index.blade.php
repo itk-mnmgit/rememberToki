a10b7580259316ca36fa243b05cbbf78c61b996a
尾宮重宏 <sigehiro.0728@gmail.com>
2019-12-13 11:42:45 +0800

    sdfg

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -20,10 +20,6 @@
         <!-- 1列目 -->
         <li>
             <a class="btn btn-blac text-white" href="{{ route('chat.listGroup') }}" role="button">＋ グループを追加する</a>
-            {{-- 消す予定一旦保留 --}}
-            {{-- @foreach($attendGroups as $attendGroup)
-            <a class="nav-link active text-light" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">{{ $attendGroup->group->name }}</a>
-            @endforeach --}}
         </li>
 
         <!-- 2列目 -->
@@ -63,11 +59,11 @@
                 <i class="fa fa-tachometer" aria-hidden="true"></i>+ 他のイベントを見る
             </a>
         </li>
+        @foreach($attendEvents as $attendEvent)
         <li>
-            @foreach($attendEvents as $attendEvent)
             <h5 class="title">{{ $attendEvent->event->name}}</h5>
-            @endforeach
         </li>
+        @endforeach
     </ul>
 </div>
 
@@ -85,57 +81,5 @@
         </div>
     </div>
 
-
-
-        {{-- 以下まだ書いてない --}}
-        {{-- イベントのバーを表示する --}}
-        {{-- <div class="col-md-3 left-content">
-            <div class="eventbar text-center">
-                <div class="event">
-                    {{-- <h3 class="text-light">参加予定のイベント</h3> --}}
-
-                    {{-- @foreach ($attendEvents as $attendEvent) --}}
-
-                        {{-- name='id' value ='{{ $attendEvent->event->id }}' でmodalにid渡したい --}}
-                        {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal-{{ $attendEvent->event->id}}" name='id' value ='{{ $attendEvent->event->id }}'>詳細</button> --}}
-                    {{-- modal --}}
-                        {{-- <div class="modal fade" id="myModal-{{ $attendEvent->event->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="title">{{ $attendEvent->event->name}}</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <img src="{{ asset($attendEvent->event->img) }}" alt="business city" class='img-fluid card-img-top'>
-                                    <div class="modal-body">
-                                        <p class="card-text">{{ $attendEvent->event->intro }}</p>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                                        <form method='post' action='{{ route('event.leave') }}'>
-                                            @csrf
-                                            <input type="hidden" name="id" value="{{ $attendEvent->event->id }}">
-                                            <button type="submit" class="btn btn-danger">このイベントから退会</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="card-text">
-                                <small class="text-muted">
-                                    {{ $attendEvent->event->startTime }} - {{ $attendEvent->event->finishTime }}
-                                </small>
-                            </p>
-                        </div>
-                    @endforeach
-                </div> --}}
-                {{-- <a class="btn btn-info btn-lg text-white" href="{{ route('event.index') }}" role="button">+ 他のイベントを見る</a> --}}
-                {{-- </div>
-        </div>
-    </div> --}}
-
-
-{{-- </div> --}}
 
 @endsection
